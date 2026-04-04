974a0e897b8851e47101b18dd7f1270efa109b2d
Nickddb <nicollerocha.dev2024>
2026-04-04 12:31:54 -0300

    Aula 7 - grid

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -2,7 +2,14 @@ function App(){
 
   return (
       <>
-        <h1 className='text-blue-500 font-graphik-bold'>Hello, World!</h1>
+        <h1 className='text-primary bg-dark-background font-graphik-bold'>Hello, World!</h1>
+        <div>
+          <div className="w-full flex gap-3 items-center flex-col lg:flex-row justify-center">
+            <div className="bg-blue-700 w-32 h-32 md:w-64 md:h-64 xs:bg-green-400"></div>
+            <div className="bg-blue-700 w-32 h-32 md:w-64 md:h-64 xl:bg-red-600"></div>
+            <div className="bg-blue-700 w-32 h-32 md:w-64 md:h-64"></div>
+          </div>
+        </div>
       </>
     )
 }
---
+++ my-project/src/Components/button/index.jsx
@@ -0,0 +1,10 @@
+import {button} from './Components/button'
+
+function app() => {
+    return (
+        <>
+            <h1>Hellow world</h1>
+            <button></button>
+        </>
+    )
+}
