a9ec4e08dbc3c56de4e55a312ac423bb40e35a4d
Nickddb <nicollerocha.dev2024>
2026-04-04 21:03:40 -0300

    Aula 8 - botões

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,3 +1,5 @@
+
+import { Button } from './Components/button';
 function App(){
 
   return (
@@ -9,6 +11,14 @@ function App(){
             <div className="bg-blue-700 w-32 h-32 md:w-64 md:h-64 xl:bg-red-600"></div>
             <div className="bg-blue-700 w-32 h-32 md:w-64 md:h-64"></div>
           </div>
+        </div>
+
+        <h1 className='text-dark-background'>Hello world</h1> 
+        <div className="bg-dark-background">
+          <Button>Primary Background</Button>
+          <Button variant='secondary'>Secondary Background</Button>
+          <Button isBold={true}>Bold Text with Primary Background</Button>
+          <Button isBold={true} variant='secondary'>Bold Text with Secondary Background</Button>
         </div>
       </>
     )
--- my-project/src/Components/button/index.jsx
+++ my-project/src/Components/button/index.jsx
@@ -1,10 +1,15 @@
-import {button} from './Components/button'
-
-function app() => {
-    return (
-        <>
-            <h1>Hellow world</h1>
-            <button></button>
-        </>
-    )
-}
+export const Button = ({ children, variant = "primary", isBold = false }) => {
+  return (
+    <button
+      className={`flex items-center justify-center p-4 rounded-[35px] text-xl leading-5 transition-all 
+        ${variant === 'primary' 
+          ? 'bg-primary text-white' 
+          : 'bg-white text-primary border border-primary'
+        } 
+        ${isBold ? 'font-graphik-bold' : 'font-graphik'}
+      `}
+    >
+      {children}
+    </button>
+  );
+};
